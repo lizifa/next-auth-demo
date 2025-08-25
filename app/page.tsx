152e5c174e8b91794b286ee54b9c75b13846db5f
小书包 <550083126@qq.com>
2025-08-25 08:58:14 +0800

    feat: test

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 mt-4">
         
         <div className="text-center space-y-4">
           
--- auth.ts
+++ auth.ts
@@ -9,30 +9,30 @@ export const config = {
       allowDangerousEmailAccountLinking: true,
     }),
   ],
-  pages: {
-    signIn: '/auth/signin',
-    signOut: '/auth/signout',
-    error: '/auth/error',
-  },
-  callbacks: {
-    async jwt({ token, account, profile }: any) {
-      // 在JWT token中添加GitHub信息
-      if (account?.provider === "github") {
-        token.accessToken = account.access_token
-      }
-      if (profile) {
-        token.id = profile.id
-      }
-      return token
-    },
-    async session({ session, token }: any) {
-      // 在session中添加用户ID
-      if (session.user) {
-        session.user.id = token.id as string
-      }
-      return session
-    },
-  },
+  // pages: {
+  //   signIn: '/auth/signin',
+  //   signOut: '/auth/signout',
+  //   error: '/auth/error',
+  // },
+  // callbacks: {
+  //   async jwt({ token, account, profile }: any) {
+  //     // 在JWT token中添加GitHub信息
+  //     if (account?.provider === "github") {
+  //       token.accessToken = account.access_token
+  //     }
+  //     if (profile) {
+  //       token.id = profile.id
+  //     }
+  //     return token
+  //   },
+  //   async session({ session, token }: any) {
+  //     // 在session中添加用户ID
+  //     if (session.user) {
+  //       session.user.id = token.id as string
+  //     }
+  //     return session
+  //   },
+  // },
   secret: process.env.AUTH_SECRET,
 }
 
