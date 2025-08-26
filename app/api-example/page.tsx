3541222250d8e8362fa4b5141f0923125e85cd2a
小书包 <550083126@qq.com>
2025-08-26 10:32:01 +0800

    feat: test

--- app/api-example/page.tsx
+++ app/api-example/page.tsx
@@ -51,7 +51,7 @@ export default function ApiExample() {
             </button>
 
             <button
-              onClick={() => signIn('github', { callbackUrl: '/api-example' })}
+              onClick={() => signIn('github', { callbackUrl: `${window.location.origin}/api-example` })}
               disabled={loading}
               className="bg-blue-500 hover:bg-blue-600 disabled:bg-gray-400 text-white px-4 py-2 rounded"
             >
@@ -59,7 +59,7 @@ export default function ApiExample() {
             </button>
 
             <button
-              onClick={() => signIn('google')}
+              onClick={() => signIn('google',{ callbackUrl: `${window.location.origin}/api-example` })}
               disabled={loading}
               className="bg-blue-500 hover:bg-blue-600 disabled:bg-gray-400 text-white px-4 py-2 rounded"
             >
